40ae1102408dd49c78ccf0835d370f880aced682
imjchiang <jchiang296@gmail.com>
2020-07-16 15:28:26 -0700

    [JS] add printArray functions and calculator functions

--- app.js
+++ app.js
@@ -17,3 +17,48 @@ function anotherGreeting()
 }
 
 console.log(greeting(myName, anotherGreeting));
+
+function printArray(array, callback)
+{
+    for (let i = 0; i < array.length; i++)
+    {
+        let number = array[i];
+        console.log(number);
+    }
+    callback(array);
+}
+
+function printArrayAgain(array)
+{
+    for (let i = 0; i < array.length; i++)
+    {
+        let number = array[i];
+        console.log(number);
+    }
+}
+
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+printArray(numbersArray, printArrayAgain);
+
+
+function calculator(num1, num2, callbackOne, callbackTwo)
+{
+    let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
+
+    return result;
+}
+
+function addNumbers(num1, num2)
+{
+    let result = num1 + num2;
+    return result;
+}
+
+function subtractNumbers(num1, num2)
+{
+    let result = num1 - num2;
+    return result;
+}
+
+console.log(calculator(5, 77, addNumbers, subtractNumbers));
